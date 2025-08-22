b2df5f4bab776dfdd6edc1665278cc726ede5580
Brice Burgess <nesta@iceburg.net>
2025-08-21 18:46:16 -0600

    :shipit: uvicorn startup improvements for dev|prod

--- src/registry.py
+++ src/registry.py
@@ -15,8 +15,10 @@ app = create_app()
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "registry:app",
         host=os.environ.get("REGISTRY_BIND_HOST", "localhost"),
-        port=int(os.environ.get("REGISTRY_BIND_PORT", 8000)),
         log_level=os.environ.get("REGISTRY_LOG_LEVEL", "info"),
+        port=int(os.environ.get("REGISTRY_BIND_PORT", 8000)),
+        reload=True,
+        reload_dirs=["src"],
     )
